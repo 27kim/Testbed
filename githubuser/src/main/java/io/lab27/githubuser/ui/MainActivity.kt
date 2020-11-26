9bd5d882d7db2bcd262be4cfb7fe900400b24151
27k <doohyeonkim@gmail.com>
2020-11-26 21:50:15 +0900

    stash test 완료

--- githubuser/src/main/java/io/lab27/githubuser/ui/MainActivity.kt
+++ githubuser/src/main/java/io/lab27/githubuser/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package io.lab27.githubuser.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -10,15 +11,17 @@ import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import io.lab27.githubuser.R
+import io.lab27.githubuser.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     lateinit var navHostFragment: NavHostFragment
     lateinit var navController: NavController
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
@@ -26,5 +29,6 @@ class MainActivity : AppCompatActivity() {
 
         appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
         toolbar.setupWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)    }
+        navView.setupWithNavController(navController)
+    }
 }
